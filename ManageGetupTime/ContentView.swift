f09b173d2147e0e11ea9917b338f4971d3b7a749
RyosukeKamimura <come1234560@gmail.com>
2020-06-25 14:38:05 +0900

    navi-menu bar completed

--- ManageGetupTime/ContentView.swift
+++ ManageGetupTime/ContentView.swift
@@ -1,21 +1,60 @@
-//
-//  ContentView.swift
-//  ManageGetupTime
-//
-//  Created by 神村亮佑 on 2020/06/24.
-//  Copyright © 2020 神村亮佑. All rights reserved.
-//
-
 import SwiftUI
+import SSCustomTabbar
 
-struct ContentView: View {
+struct SSCustomTabViewSample: View {
+    @State var isTabBarHidden:Bool = false
+    
     var body: some View {
-        Text("Hello, World!")
+        tabView
+            .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+                SwiftUITabBarController.refreshViews()
+        }
+    }
+    var tabView: some View {
+        let vc1 = SwiftUITabView(
+            content: UIHostingController(rootView: TabItem_Sample(backColor: Color.red,
+                                                                  foreColor: Color.blue)),
+            title: "Home",
+            selectedImage: "iconHomeSelected",
+            unSelectedImage: "iconHome")
+        
+        let vc2 = SwiftUITabView(
+            content: UIHostingController(rootView: TabItem_Sample(backColor: Color.black,
+                                                                  foreColor: Color.yellow)),
+            title: "Favorite",
+            selectedImage: "iconFavoriteSelected",
+            unSelectedImage: "iconFavorite")
+        
+        let vc3 = SwiftUITabView(
+            content: UIHostingController(rootView:
+                TabItem_Sample(backColor: Color.white,
+                               foreColor: Color.green)),
+            title: "Profile",
+            selectedImage: "iconProfileSelected",
+            unSelectedImage: "iconProfile")
+        
+        let tabBarView = SwiftUITabBarController(tabItems: [vc1,vc2,vc3], configuration: .constant(SSTabConfiguration()), isTabBarHidden: self.$isTabBarHidden)
+        
+        return tabBarView
+    }
+    
+}
+
+struct TabItem_Sample: View {
+    @State var backColor:Color
+    @State var foreColor:Color
+    
+    var body: some View {
+        ZStack{
+            self.backColor.edgesIgnoringSafeArea(.all)
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .foregroundColor(self.foreColor)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
     }
 }
